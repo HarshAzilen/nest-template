8ad866c0af5ef0eeecbc3fca70e4d6269ab6ac43
Dixit Vora <dixit.vora@azilen.com>
2024-05-01 19:29:31 +0530

    feat: added the pino logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,43 @@ import { ConfigModule } from '@nestjs/config';
 import { PostsModule } from './posts/posts.module';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
-
+import { LoggerModule } from 'nestjs-pino';
+const levels = {
+  http: 10,
+  debug: 20,
+  info: 30,
+  warn: 40,
+  error: 50,
+  fatal: 60,
+};
 @Module({
   imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        customProps: (_req, _res) => ({
+          context: 'HTTP',
+        }),
+        level: 'trace',
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            ignore: 'pid,hostname',
+            singleLine: true,
+            customLevels: levels,
+            useOnlyCustomLevels: true,
+            colorize: true,
+            levelFirst: true,
+            translateTime: 'yyyy-dd-mm, h:MM:ss TT',
+          },
+        },
+      },
+    }),
     ConfigModule.forRoot({ ignoreEnvFile: false, envFilePath: '.env' }),
     DatabaseModule,
     AuthModule,
     PostsModule,
   ],
   controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,13 @@ import { AppModule } from './app.module';
 import { ResponseFormatInterceptor } from './utils/response-format.interceptor';
 import { ExceptionFormatFilter } from './utils/exception-format.filter';
 import { DomainExceptionInterceptor } from './utils/domain-exception.filter';
+import { Logger, LoggerErrorInterceptor, LoggerModule, PinoLogger } from 'nestjs-pino';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
   const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useLogger(app.get(Logger));
+  app.useGlobalInterceptors(new LoggerErrorInterceptor());
 
   app.useGlobalFilters(new ExceptionFormatFilter(httpAdapter));
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
@@ -24,8 +27,10 @@ async function bootstrap(): Promise<void> {
 
   const configService = app.get<ConfigService>(ConfigService);
   const expressInstance = app.getHttpAdapter().getInstance();
-
-  await app.listen(3000);
+  await app.listen(configService.get<number>('PORT') || 3000, () => {
+    PinoLogger.prototype.info('Server is running on Port:' + configService.get<number>('PORT'));
+    PinoLogger.prototype.info('http://localhost:' + configService.get<number>('PORT'));
+  });
 }
 
 void bootstrap();
